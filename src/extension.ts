eafd1a0fdaf87f83829f264e7074fc1989d6cb1f
dilantha111 <dilantha.prasanjith111@gmail.com>
2017-09-13 01:11:31 +0530

    Adding Tree data view to list issues

--- src/extension.ts
+++ src/extension.ts
@@ -4,16 +4,20 @@ import * as vscode from 'vscode';
 import { workspace } from "vscode";
 
 import {Issues} from "./Issues";
+import { IssueItemProvideer } from "./issueTreeProvider";
 
 export function activate(context: vscode.ExtensionContext) {
     console.log('Extension activated ...')
 
     let disposable = vscode.commands.registerCommand('extension.ghc', () => {
    
-        const issues = new Issues()
-        issues.getIssues(workspace.workspaceFolders[0].uri.path).then(res=>{
+        const issues = new Issues(workspace.workspaceFolders[0].uri.path)
+        issues.getIssues().then(res=>{
             console.log(res)
         })
+
+        const issueItemProvider = new IssueItemProvideer(issues)
+        vscode.window.registerTreeDataProvider('ghc.issue.treeView',issueItemProvider)
 
         console.log(workspace.workspaceFolders[0].uri.path)
         vscode.window.showInformationMessage('ghc command issued ');
---
+++ src/issueTreeProvider.ts
@@ -0,0 +1,53 @@
+import * as vscode from 'vscode';
+import { Issues } from "./Issues";
+
+export class IssueItemProvideer implements vscode.TreeDataProvider<IssueItem>{
+    private _onDidChangeTreeData: vscode.EventEmitter<IssueItem | undefined> = new vscode.EventEmitter<IssueItem | undefined>();
+    readonly onDidChangeTreeData: vscode.Event<IssueItem | undefined> = this._onDidChangeTreeData.event;
+
+    constructor(private issues: Issues) {
+    }
+
+    refresh(): void {
+        this._onDidChangeTreeData.fire();
+    }
+
+    getTreeItem(element: IssueItem): vscode.TreeItem {
+        return element;
+    }
+
+    getChildren(element?: IssueItem): Thenable<IssueItem[]> {
+        if (element) {
+            return Promise.resolve([])
+        } else {
+            return this._getIssues()
+        }
+    }
+
+    private _getIssues() {
+        return this.issues.getIssues().then(res => {
+            return res.map((_issue) => {
+                let label = _issue.title + ' #' + _issue.id
+                /* Add a command to execute when item is selected */
+                return new IssueItem(label, vscode.TreeItemCollapsibleState.None)
+            })
+        })
+    }
+}
+
+class IssueItem extends vscode.TreeItem {
+    constructor(
+        public readonly label: string,
+        public readonly collapsibleState: vscode.TreeItemCollapsibleState,
+        public readonly command?: vscode.Command
+    ) {
+        super(label, collapsibleState);
+    }
+
+    // iconPath = {
+    // 	light: path.join(__filename, '..', '..', '..', 'resources', 'light', 'dependency.svg'),
+    // 	dark: path.join(__filename, '..', '..', '..', 'resources', 'dark', 'dependency.svg')
+    // };
+
+    contextValue = 'issue';
+}
